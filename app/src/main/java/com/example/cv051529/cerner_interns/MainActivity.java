159f7f42ffe674de3569c3861dca206f5a73f2a2
Jackson Voigt <jackson.voigt@cerner.com>
2017-06-19 21:06:28 -0500

    Fixed drawer stuff

--- app/src/main/java/com/example/cv051529/cerner_interns/MainActivity.java
+++ app/src/main/java/com/example/cv051529/cerner_interns/MainActivity.java
@@ -77,26 +77,19 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        if (id == R.id.event) {
             EventFragment fragment = new EventFragment();
             fragment.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-        } else if (id == R.id.nav_send) {
-
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+        } else if (id == R.id.question) {
+            EventFragment fragment = new EventFragment();
+            fragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
